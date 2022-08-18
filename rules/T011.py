db4287e079a754a3b41ec35534258ade875ed75b
Jia-Yin <jywglady@gmail.com>
2022-08-18 12:45:00 +0800

    PY Rules Ver. 1

--- rules/T011.py
+++ rules/T011.py
@@ -25,16 +25,44 @@ for f in vera.getSourceFileNames():
     ok, pdicts = paringBrace(f, tokens)
     if not ok:
         continue
+    
     for i in range(len(tokens)):
         if tokens[i].type != "leftbrace":
             continue
         leftP, rightP = tokens[i], pdicts[tokens[i]]
         if leftP.line == rightP.line: # Both at the same line
             continue
+
         # Below is the case: left and right are at different lines
+
         # leftP 前面為 = 或 { 時忽略規則
+        # skip space
+        j = i-1
+        while j>=0 and tokens[j].type == "space":
+            j -= 1
+        # check
+        if tokens[j].line == leftP.line and tokens[j].type in ["assign", "leftbrace"]:
+            continue
 
+        # left and right at different columns, 左邊大括號後面只能放空白及註解
+        if leftP.column != rightP.column:
+            ok = True
+            j = i+1
+            while j<len(tokens) and tokens[j].line == leftP.line:
+                if not tokens[j].type in ["space", "ccomment", "cppcomment", "newline"]:
+                    ok = False
+                    break
+                else:
+                    j += 1
+            if not ok:
+                vera.report(f, leftP.line, "多列的區塊，左邊大括號後面應該換列")
 
-        print(leftP.line, leftP.column, rightP.line, rightP.column)
+        for lineno in range(leftP.line+1, rightP.line):
+            lineTokens = vera.getTokens(f, lineno, 0, lineno+1, 0, [])
+            for k in range(len(lineTokens)):
+                if not lineTokens[k].type in ["space", "newline"]:
+                    if lineTokens[k].column <= rightP.column:
+                        vera.report(f, lineno, "程式碼未正確縮排")
+                    break
 
 
